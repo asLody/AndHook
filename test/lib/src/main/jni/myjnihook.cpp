f4be3dc488b1dc616fcfaadd20f19472527a03a5
RLibEx <rrrfff@foxmail.com>
2017-11-06 23:44:22 +0800

    Upgrade to gradle 3.0

--- test/lib/src/main/jni/myjnihook.cpp
+++ test/lib/src/main/jni/myjnihook.cpp
@@ -2,13 +2,13 @@
 #include <android/log.h>
 #include <alloca.h>
 #include "include/AndHook.h"
-#define MSHook(X) MSHookFunction(reinterpret_cast<void *>(X), reinterpret_cast<void *>(my_##X), reinterpret_cast<void **>(&sys_##X));
+#define AKHook(X) AKHookFunction(reinterpret_cast<void *>(X), reinterpret_cast<void *>(my_##X), reinterpret_cast<void **>(&sys_##X));
 
 static intptr_t sys_getGTalkDeviceId;
 
 static jstring JNICALL my_getGTalkDeviceId(JNIEnv *env, jclass obj, jlong j)
 {
-	jmethodID jm = MSGetMethodID(env, sys_getGTalkDeviceId, alloca(JNI_METHOD_SIZE));
+	jmethodID jm = AKGetMethodID(env, sys_getGTalkDeviceId, alloca(JNI_METHOD_SIZE));
 	jstring   js = reinterpret_cast<jstring>(env->CallStaticObjectMethod(obj, jm, j));
 	if (js != NULL) {
 		__android_log_print(ANDROID_LOG_INFO, __FUNCTION__, "%s", env->GetStringUTFChars(js, NULL));
@@ -39,21 +39,21 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 
     __android_log_print(ANDROID_LOG_INFO, __FUNCTION__, "starting jni hook...");
     jclass clazz = env->FindClass("android/provider/Settings");
-    MSJavaHookMethod(env, clazz, "getGTalkDeviceId", "(J)Ljava/lang/String;",
+    AKJavaHookMethod(env, clazz, "getGTalkDeviceId", "(J)Ljava/lang/String;",
                      reinterpret_cast<void *>(my_getGTalkDeviceId), &sys_getGTalkDeviceId);
     /* the following code is equivalent to call MSJavaHookMethod/MSJavaHookMethodV
     jmethodID md = env->GetStaticMethodID(clazz,
                                           "getGTalkDeviceId", "(J)Ljava/lang/String;");
     void **jni_entrypoint = NULL;
-    sys_getGTalkDeviceId = MSForceNativeMethod(md, &jni_entrypoint, true);
+    sys_getGTalkDeviceId = AKForceNativeMethod(md, &jni_entrypoint, true);
     if (jni_entrypoint != NULL) *jni_entrypoint = reinterpret_cast<void *>(my_getGTalkDeviceId);
     */
     __android_log_print(ANDROID_LOG_INFO, __FUNCTION__, "jni hook done.");
 
     __android_log_print(ANDROID_LOG_INFO, __FUNCTION__, "starting native hook...");
-    // MSSuspendAllThreads();
-    MSHook(access);
-    // MSResumeAllThreads();
+    // AKSuspendAllThreads();
+    AKHook(access);
+    // AKResumeAllThreads();
     __android_log_print(ANDROID_LOG_INFO, __FUNCTION__, "native hook done.");
 
     return JNI_VERSION_1_6;
