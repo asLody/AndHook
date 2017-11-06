24f5db497a7e87d17cd5a46052aa7014585ee6a1
RLibEx <rrrfff@foxmail.com>
2017-11-06 23:35:25 +0800

    Use new prefix AK_ to escape from potential detection

--- jni/include/AndHook.h
+++ jni/include/AndHook.h
@@ -20,53 +20,53 @@ extern "C" {
 	/// <summary>
 	/// Gets handle to specified module or NULL if the target is not currently loaded
 	/// </summary>
-	const void *MSGetImageByName(const char *name/* = ANDROID_RUNTIME*/);
+	const void *AKGetImageByName(const char *name/* = ANDROID_RUNTIME*/);
 	/// <summary>
 	/// Gets the addresses of defined symbols or NULL
 	/// </summary>
-	void *MSFindSymbol(const void *handle, const char *symbol);
+	void *AKFindSymbol(const void *handle, const char *symbol);
 	/// <summary>
 	/// Releases internal memory without affecting the module state
 	/// </summary>
-	void MSCloseImage(const void *handle);
+	void AKCloseImage(const void *handle);
 	/// <summary>
 	/// Intercepts native methods
 	/// </summary>
-	void MSHookFunction(void *symbol, void *replace, void **result/* = NULL*/);
+	void AKHookFunction(void *symbol, void *replace, void **result/* = NULL*/);
 	/// <summary>
 	/// Intercepts java methods using native code
 	/// </summary>
-	void MSJavaHookMethod(JNIEnv *env, jclass clazz, const char *method, const char *signature,
+	void AKJavaHookMethod(JNIEnv *env, jclass clazz, const char *method, const char *signature,
 						  void *replace, intptr_t *result/* = NULL*/);
 	/// <summary>
 	/// Intercepts java methods using native code 
 	/// </summary>
-	void MSJavaHookMethodV(JNIEnv *env, jclass clazz, jmethodID methodId, const char *method, const char *signature,
+	void AKJavaHookMethodV(JNIEnv *env, jclass clazz, jmethodID methodId, const char *method, const char *signature,
 						   void *replace, intptr_t *result/* = NULL*/);
 	/// <summary>
 	/// Gets the underlying jmethodID which can be used to call original function
 	/// </summary>
-	jmethodID MSGetMethodID(JNIEnv *env, intptr_t backup, void *buffer/* JNI_METHOD_SIZE */);
+	jmethodID AKGetMethodID(JNIEnv *env, intptr_t backup, void *buffer/* JNI_METHOD_SIZE */);
 	/// <summary>
 	/// Marks the specified java method as native (if not) and returns backup-slot index
 	/// </summary>
-	intptr_t MSForceNativeMethod(jmethodID origin, void ***jni_entrypoint, bool should_backup);
+	intptr_t AKForceNativeMethod(jmethodID origin, void ***jni_entrypoint, bool should_backup);
 	/// <summary>
 	/// Forces the specified java method to be executed in the interpreter [ART only]
 	/// </summary>
-	void MSDeoptimizeMethod(jmethodID method);
+	void AKDeoptimizeMethod(jmethodID method);
 	/// <summary>
 	/// Dumps all the virtual and direct methods of the specified class
 	/// </summary>
-	void MSDumpClassMethods(JNIEnv *env, jclass clazz/* = NULL*/, const char *clsname/* = NULL*/);
+	void AKDumpClassMethods(JNIEnv *env, jclass clazz/* = NULL*/, const char *clsname/* = NULL*/);
 	/// <summary>
 	/// Ensures all threads running Java suspend and that those not running Java don't start
 	/// </summary>
-	bool MSLockJavaThreads();
+	bool AKLockJavaThreads();
 	/// <summary>
-	/// Unlocks and broadcasts a notification to all threads interrupted by MSLockJavaThreads
+	/// Unlocks and broadcasts a notification to all threads interrupted by AKLockJavaThreads
 	/// </summary>
-	void MSUnlockJavaThreads();
+	void AKUnlockJavaThreads();
 	/// <summary>
 	/// Suspends all threads in the current process except the calling one.
 	/// This function works by spawning a Linux task which then attaches to every
@@ -74,19 +74,19 @@ extern "C" {
 	/// @warning calling a library function while threads are suspended could cause a
 	/// deadlock, if one of the treads happens to be holding a libc lock
 	/// </summary>
-	bool MSSuspendAllThreads();
+	bool AKSuspendAllThreads();
 	/// <summary>
-	/// Resumes all threads suspended by MSSuspendAllThreads
+	/// Resumes all threads suspended by AKSuspendAllThreads
 	/// </summary>
-	void MSResumeAllThreads();
+	void AKResumeAllThreads();
 	/// <summary>
 	/// Returns the absolute path to the application specific cache directory with trailing slash
 	/// </summary>
-	const char *MSGetCacheDirectory();
+	const char *AKGetCacheDirectory();
 	/// <summary>
 	/// Enables or disables dex fast-loading mechanism [ART only]
 	/// </summary>
-	void MSEnableFastDexLoad(bool enable);
+	void AKEnableFastDexLoad(bool enable);
 
 #ifdef __cplusplus
 }
